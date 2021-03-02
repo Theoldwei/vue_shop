d406cab4d98cbd01b91c87cabea27cfb7fbd5549
The old wei <1814687573@qq.com>
2021-03-02 20:26:56 +0800

    完成了商品功能

--- src/main.js
+++ src/main.js
@@ -9,6 +9,14 @@ import './assets/css/global.css'
 import TreeTable from 'vue-table-with-tree-grid'
 
 import axios from 'axios'
+
+//导入vue-quill-editor（富文本编辑器）
+import VueQuillEditor from 'vue-quill-editor'
+//导入vue-quill-editor的样式
+import 'quill/dist/quill.core.css'
+import 'quill/dist/quill.snow.css'
+import 'quill/dist/quill.bubble.css'
+
 // 配置请求的跟路径
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
 // axios请求拦截器，每次发送请求前都需要携带token并由后端判断是否响应，以保证获取数据的权限
@@ -23,6 +31,22 @@ Vue.prototype.$http = axios
 Vue.config.productionTip = false
 
 Vue.component('tree-table', TreeTable)
+
+//全局注册富文本组件
+Vue.use(VueQuillEditor)
+
+//创建过滤器将秒数过滤为年月日，时分秒
+Vue.filter('dateFormat', function (originVal) {
+  const dt = new Date(originVal)
+  const y = dt.getFullYear()
+  const m = (dt.getMonth() + 1 + '').padStart(2, '0')
+  const d = (dt.getDate() + '').padStart(2, '0')
+  const hh = (dt.getHours() + '').padStart(2, '0')
+  const mm = (dt.getMinutes() + '').padStart(2, '0')
+  const ss = (dt.getSeconds() + '').padStart(2, '0')
+
+  return `${y}-${m}-${d} ${hh}:${mm}:${ss}`
+})
 
 new Vue({
   router,
